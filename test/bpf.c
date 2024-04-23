cad9b639e6a6ed64f962fce391239aab78c96750
Farbod Shahinfar <fshahinfar1@gmail.com>
2024-04-23 09:36:10 +0000

    add a test file

---
+++ test/bpf.c
@@ -0,0 +1,37 @@
+/* vim: set et ts=2 sw=2: */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <linux/in.h>
+#include <linux/tcp.h>
+
+/* Put state of each socket in this struct (This will be used in sockops.h as
+ * part of per socket metadata) */
+struct connection_state { };
+#include "sockops.h"
+
+SEC("sk_skb/stream_parser")
+int parser(struct __sk_buff *skb)
+{
+  bpf_printk("parser");
+  return skb->len;
+}
+
+SEC("sk_skb/stream_verdict")
+int verdict(struct __sk_buff *skb)
+{
+  bpf_printk("here");
+  if (bpf_skb_pull_data(skb, skb->len) != 0) {
+    bpf_printk("Parser: Failed to load message data");
+    return SK_DROP;
+  }
+  void *data = (void *)((__u64)skb->data);
+  void *data_end = (void *)((__u64)skb->data_end);
+  __u16 len = skb->len;
+  bpf_printk("recv: %d bytes", len);
+  return (SK_DROP);
+}
+
+char _license[] SEC("license") = "GPL";
---
+++ test/run.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+# Farbod Shahinfar 2024
+if [ -z "$KASHK_DIR" ]; then
+	echo "KASHK_DIR is not defined"
+	exit 1
+fi
+if [ -z "$NET_IFACE" ]; then
+	echo "Missing the NET_IFACE"
+	exit 1
+fi
+
+CURDIR=$(realpath $(dirname $0))
+BPF_GEN_DIR=$KASHK_DIR
+BPF_GEN=$KASHK_DIR/bpf_gen.sh
+BPF_COMPILE_SCRIPT=$BPF_GEN_DIR/compile_scripts/compile_bpf_source.sh
+
+BUILD_DIR=$CURDIR/build
+BPF_BINARY=/$BUILD_DIR/_test_bpf.o
+BPF_GENERATED_FILE=$CURDIR/bpf.c
+
+
+mkdir -p $BUILD_DIR/
+
+
+if [ -f $BPF_BINARY ]; then rm -f $BPF_BINARY; fi
+
+# Compile the BPF program
+bash $BPF_COMPILE_SCRIPT $BPF_GENERATED_FILE $BPF_BINARY
+
+# Test if it loads into the kernel
+# bash $BPF_GEN_DIR/compile_scripts/load.sh $BPF_BINARY
+
+# Load and run the BPF program
+sudo $BPF_GEN_DIR/compile_scripts/loader -b $BPF_BINARY --skskb verdict
